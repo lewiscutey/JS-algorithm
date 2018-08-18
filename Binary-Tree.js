56ac77ba57012eb0ef68e660d8cd7ea12015bd10
吕新 <949124363@qq.com>
2018-08-18 22:35:23 +0800

    fix format

--- Binary-Tree.js
+++ Binary-Tree.js
@@ -1,201 +1,198 @@
 function BinarySearchTree() {
-  var Node = function(key) {
-    (this.key = key), (this.left = null), (this.right = null);
-  };
-
-  var root = null;
-
-  //插入节点
-
-  this.insert = function(key) {
-    var newNode = new Node(key);
-
-    if (root === null) {
-      root = newNode;
-    } else {
-      insertNode(root, newNode);
-    }
-  };
-
-  var insertNode = function(node, newNode) {
-    if (newNode.key <= node.key) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        insertNode(node.right, newNode);
-      }
-    }
-  };
-
-  //实现中序遍历
-
-  this.inOrderTraverse = function() {
-    inOrderTraverseNode(root);
-  };
-
-  var inOrderTraverseNode = function(node) {
-    if (node !== null) {
-      inOrderTraverseNode(node.left);
-      console.log(node.key);
-      inOrderTraverseNode(node.right);
-    }
-  };
-
-  // 实现先序遍历
-
-  this.preOrderTraverse = function() {
-    preOrderTraverseNode(root);
-  };
-
-  var preOrderTraverseNode = function(node) {
-    if (node !== null) {
-      console.log(node.key);
-      preOrderTraverseNode(node.left);
-      preOrderTraverseNode(node.right);
-    }
-  };
-
-  // 实现后序遍历
-
-  this.postOrderTraverse = function() {
-    postOrderTraverseNode(root);
-  };
-
-  var postOrderTraverseNode = function(node) {
-    if (node !== null) {
-      postOrderTraverseNode(node.left);
-      postOrderTraverseNode(node.right);
-      console.log(node.key);
-    }
+	var Node = function (key) {
+		(this.key = key), (this.left = null), (this.right = null);
 	};
-	
+
+	var root = null;
+
+	//插入节点
+
+	this.insert = function (key) {
+		var newNode = new Node(key);
+
+		if (root === null) {
+			root = newNode;
+		} else {
+			insertNode(root, newNode);
+		}
+	};
+
+	var insertNode = function (node, newNode) {
+		if (newNode.key <= node.key) {
+			if (node.left === null) {
+				node.left = newNode;
+			} else {
+				insertNode(node.left, newNode);
+			}
+		} else {
+			if (node.right === null) {
+				node.right = newNode;
+			} else {
+				insertNode(node.right, newNode);
+			}
+		}
+	};
+
+	//实现中序遍历
+
+	this.inOrderTraverse = function () {
+		inOrderTraverseNode(root);
+	};
+
+	var inOrderTraverseNode = function (node) {
+		if (node !== null) {
+			inOrderTraverseNode(node.left);
+			console.log(node.key);
+			inOrderTraverseNode(node.right);
+		}
+	};
+
+	// 实现先序遍历
+
+	this.preOrderTraverse = function () {
+		preOrderTraverseNode(root);
+	};
+
+	var preOrderTraverseNode = function (node) {
+		if (node !== null) {
+			console.log(node.key);
+			preOrderTraverseNode(node.left);
+			preOrderTraverseNode(node.right);
+		}
+	};
+
+	// 实现后序遍历
+
+	this.postOrderTraverse = function () {
+		postOrderTraverseNode(root);
+	};
+
+	var postOrderTraverseNode = function (node) {
+		if (node !== null) {
+			postOrderTraverseNode(node.left);
+			postOrderTraverseNode(node.right);
+			console.log(node.key);
+		}
+	};
 
 	// 查找最小值
-    
-	this.findMin = function() {
-    return;
-    minNode(root);
-  };
 
-  var minNode = function(node) {
-    if (node) {
-      while (node && node.left !== null) {
-        node = node.left;
-      }
-      return;
-      node.key;
-    }
-    return;
-    null;
-  };
+	this.findMin = function () {
+		return;
+		minNode(root);
+	};
 
-  // 查找最大值
+	var minNode = function (node) {
+		if (node) {
+			while (node && node.left !== null) {
+				node = node.left;
+			}
+			return;
+			node.key;
+		}
+		return;
+		null;
+	};
 
-  this.findMax = function() {
-    return;
-    maxNode(root);
-  };
+	// 查找最大值
 
-  var maxNode = function(node) {
-    if (node) {
-      while (node && node.right !== null) {
-        node = node.right;
-      }
-      return;
-      node.key;
-    }
-    return;
-    null;
-  };
+	this.findMax = function () {
+		return;
+		maxNode(root);
+	};
+
+	var maxNode = function (node) {
+		if (node) {
+			while (node && node.right !== null) {
+				node = node.right;
+			}
+			return;
+			node.key;
+		}
+		return;
+		null;
+	};
 
 	// 查找特定值
 
-	this.search = function(key) {
-    return;
-    searchNode(root, key);
+	this.search = function (key) {
+		return;
+		searchNode(root, key);
 	};
-	
-	var searchNode = function(node, key) {
-    if (node === null) {
-      return;
-      false;
-    }
-    if (key < node.key) {
-      return;
-      searchNode(node.left, key);
-    } else if (key > node.key) {
-      return;
-      searchNode(node.right, key);
-    } else {
-      return;
-      true;
-    }
+
+	var searchNode = function (node, key) {
+		if (node === null) {
+			return;
+			false;
+		}
+		if (key < node.key) {
+			return;
+			searchNode(node.left, key);
+		} else if (key > node.key) {
+			return;
+			searchNode(node.right, key);
+		} else {
+			return;
+			true;
+		}
 	};
-	
 
 	// 移除节点
-    
-	this.remove = function(key) {
-    removeNode(root, key);
-  };
 
-  var removeNode = function(node, key) {
-    if (node === null) {
-      return;
-      null;
-    }
-    if (key < node.key) {
-      node.left = removeNode(node.left, key);
-      return;
-      node;
-    } else if (key > node.key) {
-      node.right = removeNode(node.right, key);
-      return;
-      node;
-    } else {
-      //需要移除的节点是一个叶子节点
-      if (node.left === null && node.right === null) {
-        node = null;
-        return;
-        node;
-      }
+	this.remove = function (key) {
+		removeNode(root, key);
+	};
 
-      //需要移除的节点包含一个子节点
-      if (node.letf === null) {
-        node = node.right;
-        return;
-        node;
-      } else if (node.right === null) {
-        node = node.left;
-        return;
-        node;
-      }
+	var removeNode = function (node, key) {
+		if (node === null) {
+			return;
+			null;
+		}
+		if (key < node.key) {
+			node.left = removeNode(node.left, key);
+			return;
+			node;
+		} else if (key > node.key) {
+			node.right = removeNode(node.right, key);
+			return;
+			node;
+		} else {
+			//需要移除的节点是一个叶子节点
+			if (node.left === null && node.right === null) {
+				node = null;
+				return;
+				node;
+			}
 
-      //需要移除的节点包含两个子节点
-      var aux = findMinNode(node.right);
-      node.key = aux.key;
-      node.right = removeNode(node.right, axu.key);
-      return;
-      node;
-    }
-  };
+			//需要移除的节点包含一个子节点
+			if (node.letf === null) {
+				node = node.right;
+				return;
+				node;
+			} else if (node.right === null) {
+				node = node.left;
+				return;
+				node;
+			}
 
-  var findMinNode = function(node) {
-    if (node) {
-      while (node && node.left !== null) {
-        node = node.left;
-      }
-      return;
-      node;
-    }
-    return;
-    null;
-  };
+			//需要移除的节点包含两个子节点
+			var aux = findMinNode(node.right);
+			node.key = aux.key;
+			node.right = removeNode(node.right, axu.key);
+			return;
+			node;
+		}
+	};
 
+	var findMinNode = function (node) {
+		if (node) {
+			while (node && node.left !== null) {
+				node = node.left;
+			}
+			return;
+			node;
+		}
+		return;
+		null;
+	};
 }
